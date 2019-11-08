676c96504371c5a9186d1a46d980f3d5a64abc8b
Bharat Kaushik <54591484+bharatkaushik9194@users.noreply.github.com>
2019-11-08 17:12:13 +0530

    pep8 coding style

--- dict.py
+++ dict.py
@@ -2,12 +2,15 @@ import json
 from difflib import get_close_matches
 
 data = json.load(open("data.json"))
+
+
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
+                   get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yn == "N":
@@ -16,6 +19,7 @@ def translate(w):
             return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
+
 
 word = input("Enter word: ")
 output = translate(word)
